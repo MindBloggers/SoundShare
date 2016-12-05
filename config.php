e2e0fdb98ebf85e9b25f42365e75f458eb20dc1b
Richmond Bentil <richmond.bentil@st.vvu.edu.gh>
2016-12-05 15:07:21 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,40 @@
+////////////////////Calling from the (include )//////////////////
+
+<?php
+error_reporting(0);
+
+$CONF = $TMPL = array();
+
+// The MySQL credentials
+$CONF['host'] = 'localhost';
+$CONF['user'] = 'root';
+$CONF['pass'] = '';
+$CONF['name'] = 'soundshare';
+
+// The Installation URL
+$CONF['url'] = 'http://localhost/Script';
+
+// The Notifications e-mail
+$CONF['email'] = 'notifications@yourdomain.com';
+
+// The themes directory
+$CONF['theme_path'] = 'themes';
+
+$action = array('admin'			=> 'admin',
+				'explore'		=> 'explore',
+				'stream'		=> 'stream',
+				'settings'		=> 'settings',
+				'messages'		=> 'messages',
+				'track'			=> 'track',
+				'playlist'		=> 'playlist',
+				'upload'		=> 'upload',
+				'recover'		=> 'recover',
+				'profile'		=> 'profile',
+				'stats'			=> 'stats',
+				'pro'			=> 'pro',
+				'notifications'	=> 'notifications',
+				'search'		=> 'search',
+				'page'			=> 'page',
+				'welcome'		=> 'welcome'
+				);
+?>
